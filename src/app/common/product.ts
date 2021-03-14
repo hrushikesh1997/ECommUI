3e1568b8bbfe38f2c8c5a28f2cc0830dec802c9a
Hrushikesh Mahapatro <hmahapat@cisco.com>
2021-03-14 09:53:38 +0530

    Products Listing

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku:string;
+    name:string;
+    description:string;
+    unitPrice:number;
+    imageUrl:string;
+    active:string;
+    unitsInStock:string;
+    dateCreated:Date;
+    lastUpdated:Date;
+}
